60ed4ad38ea382442fb988e4a7618168548b4ed1
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-08-06 19:46:46 +0100

    update NoteSchema to remove the id and date fields from the schema since they will be automatically generated by the database

--- app/schemas/note.py
+++ app/schemas/note.py
@@ -2,9 +2,6 @@ from marshmallow import Schema, fields
 
 
 class NoteSchema(Schema):
-    id = fields.Int(dump_only=True)
     title = fields.Str(required=True)
     body = fields.Str(required=True)
     tags = fields.Str()
-    date = fields.DateTime(dump_only=True)
-    user_id = fields.Int(dump_only=True)

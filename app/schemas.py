bb563b160b299ef96e1b5149ddbf86b5a7821516
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-09-01 11:50:59 +0100

    Added custom schemas for better data serialization and deserialization.

--- app/schemas.py
+++ app/schemas.py
@@ -28,6 +28,23 @@ class NoteUpdateSchema(Schema):
     content = fields.Str()
 
 
+class NoteListSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    content = fields.Str()
+    date = fields.DateTime()
+    user_id = fields.Int()
+    user = fields.Nested(UserSchema())
+
+
+class NoteListResponseSchema(Schema):
+    notes = fields.List(fields.Nested(NoteListSchema))
+    page = fields.Int()
+    per_page = fields.Int()
+    total_pages = fields.Int()
+    total_notes = fields.Int()
+
+
 class NoteTagSchema(Schema):
     message = fields.Str()
     note_id = fields.Int(required=True, load_only=True)

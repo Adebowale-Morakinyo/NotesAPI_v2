2a59dc453478739b96219ad0b6837bb711b166a6
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-09-10 23:20:48 +0100

    Added code to populate the users, notes and tags tables with sample data. Linked tags to notes and saved to the database.

---
+++ populate_db.py
@@ -0,0 +1,49 @@
+from app import create_app
+from app.models.user import User
+from app.models.note import Note
+from app.models.tag import Tag
+from app.models.note_tags import note_tags
+from faker import Faker
+import random
+
+# Create a Flask app context
+app = create_app()
+
+# Initialize the app context
+app.app_context().push()
+
+# Initialize the database
+from app.db import db
+db.init_app(app)
+
+# Create a Faker instance for generating fake data
+fake = Faker()
+
+# Create users and add notes for each user
+for _ in range(5):  # Create 5 users
+    user = User(
+        username=fake.user_name(),
+        email=fake.email(),
+        full_name=fake.name(),
+        password=fake.password(),  # You might want to hash the password
+    )
+    db.session.add(user)
+
+    for _ in range(random.randint(1, 5)):  # Add random notes for each user
+        note = Note(
+            title=fake.sentence(),
+            content=fake.paragraph(),
+            user=user,
+        )
+        db.session.add(note)
+
+        # Add random tags to the note
+        for _ in range(random.randint(1, 3)):
+            tag_name = fake.word()
+            tag = Tag.query.filter_by(name=tag_name).first()
+            if not tag:
+                tag = Tag(name=tag_name)
+            note.tags.append(tag)
+
+# Commit the changes to the database
+db.session.commit()
